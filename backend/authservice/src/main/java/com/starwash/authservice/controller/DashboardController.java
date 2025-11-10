479baf7ab26b08f9479be55c31ba24911682c94b
YtterStick <133835788+YtterStick@users.noreply.github.com>
2025-11-11 01:21:48 +0800

    test 2

--- backend/authservice/src/main/java/com/starwash/authservice/controller/DashboardController.java
+++ backend/authservice/src/main/java/com/starwash/authservice/controller/DashboardController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/dashboard")
+@RequestMapping("/api/dashboard")
 @CrossOrigin(origins = "http://localhost:3000")
 public class DashboardController {
 
